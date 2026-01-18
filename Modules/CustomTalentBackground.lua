c2f8cc5ca175e8fed334085a31326d95f6dba64f
krealle <vollmer@archon.gg>
2026-01-19 00:07:26 +0100

    fatspyrowide

---
+++ Modules/CustomTalentBackground.lua
@@ -0,0 +1,27 @@
+local addonName, Private = ...
+
+EventUtil.ContinueOnAddOnLoaded("Blizzard_PlayerSpells", function()
+    -- Evoker Tech
+    if select(2, UnitClass("player")) ~= "EVOKER" then return end
+
+	local tempFrame = CreateFrame("Frame", nil, PlayerSpellsFrame)
+    local frameParent = tempFrame:GetParent()
+
+    frameParent.Bg:Hide()
+
+    frameParent.TalentsFrame.Background:SetAlpha(0.95)
+    frameParent.TalentsFrame.BlackBG:Hide()
+    frameParent.TalentsFrame.AirParticlesClose:SetAlpha(0)
+    frameParent.TalentsFrame.AirParticlesFar:SetAlpha(0)
+    frameParent.TalentsFrame.Clouds1:SetAlpha(0)
+    frameParent.TalentsFrame.Clouds2:SetAlpha(0)
+    frameParent.TalentsFrame.backgroundAnims = nil
+
+    frameParent.backgroundPath = "Interface\\Addons\\VollFix\\Textures\\fatspyrowide.tga"
+
+    local resetTextScript = function(self)
+        self.Background:SetTexture(self:GetParent().backgroundPath)
+    end
+
+    frameParent.TalentsFrame:HookScript("OnShow", resetTextScript)
+end)
