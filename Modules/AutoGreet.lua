5afaf0dc488833dc0544d2a5c3c48a3a10c2570f
krealle <vollmer@archon.gg>
2026-02-26 22:18:52 +0100

    morning

--- Modules/AutoGreet.lua
+++ Modules/AutoGreet.lua
@@ -1,30 +1,30 @@
 local addonName, Private = ...
 
 EventRegistry:RegisterFrameEventAndCallback("GROUP_JOINED", function()
-	if IsInRaid() then
-		return false
-	end
+    if IsInRaid() then
+        return false
+    end
 
-	local function DoGreeting()
-		if not InCombatLockdown() then
-			local num = math.random(1, 100)
-			local greeting = "hi"
-			if num <= 5 then
-				greeting = "hello everypony"
-			elseif num <= 10 then
-				greeting = "meowdy"
-			end
+    local function DoGreeting()
+        if not InCombatLockdown() then
+            local num = math.random(1, 100)
+            local greeting = "morning"
+            if num <= 5 then
+                greeting = "hello everypony"
+            elseif num <= 10 then
+                greeting = "meowdy"
+            end
 
-			C_ChatInfo.SendChatMessage(greeting, "PARTY")
-		end
-	end
+            C_ChatInfo.SendChatMessage(greeting, "PARTY")
+        end
+    end
 
-	if InCombatLockdown() then
-		EventRegistry:RegisterFrameEventAndCallback("PLAYER_REGEN_ENABLED", function(ownerId)
-			DoGreeting()
-			EventRegistry:UnregisterFrameEventAndCallback("PLAYER_REGEN_ENABLED", ownerId)
-		end)
-	else
-		C_Timer.After(2, DoGreeting)
-	end
+    if InCombatLockdown() then
+        EventRegistry:RegisterFrameEventAndCallback("PLAYER_REGEN_ENABLED", function(ownerId)
+            DoGreeting()
+            EventRegistry:UnregisterFrameEventAndCallback("PLAYER_REGEN_ENABLED", ownerId)
+        end)
+    else
+        C_Timer.After(2, DoGreeting)
+    end
 end)
